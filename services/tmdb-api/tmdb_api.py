0d5da693be0bd70182f942b99455aa63fd929247
encarnac <encarnac@oregonstate.edu>
2022-07-28 10:29:28 -0700

    Debugging tmdb_api

--- services/tmdb-api/tmdb_api.py
+++ services/tmdb-api/tmdb_api.py
@@ -1,4 +1,7 @@
 import requests
+IMDB_ID_SAMPLE = "tt0436992"
+CATEGORY = "TV"
+
 
 class tmdb_api:
     def __init__(self, imdb_id, category="TV"):
@@ -7,34 +10,32 @@ class tmdb_api:
         self.imdb_id = imdb_id
         self.category = category
         self.response = self.find()
-        # self.get_season_ep(self.response['number_of_seasons'])
-        # json.dump(self.main(), f"{r_file}.json")
 
     def find(self):
-      """
-      Returns the content's TMDB id to retrieve and return the film/series' relevant data 
-        Required parameters: 
-          api_key,
-          external_id (from IMDB),
-          external_source (accepted string option)
-        Returns:
-          The TMDB id and requested film or series' details after calling 
-          either movie_details() or tv_details()
-      """
-      # Uses the known IMDB id to get the TMDB id
-      self.params['external_source'] = 'imdb_id'
-      r = requests.get(
-        self.URL+'find/'+self.imdb_id,
-        params=self.params
+        """
+        Returns the content's TMDB id to retrieve and return the film/series' relevant data 
+          Required parameters: 
+            api_key,
+            external_id (from IMDB),
+            external_source (accepted string option)
+          Returns:
+            The TMDB id and requested film or series' details after calling 
+            either movie_details() or tv_details()
+        """
+        # Uses the known IMDB id to get the TMDB id
+        self.params['external_source'] = 'imdb_id'
+        r = requests.get(
+            self.URL+'find/'+self.imdb_id,
+            params=self.params
         ).json()
-        
+
         # Gets the TMDB id for a film and uses it to get more data from movie_details()
         if self.category == "FT":
             self.id = r['movie_results'][0]['id']
             info = self.movie_details(self.id)
             return info
 
-        # Gets the TMDB id for a series and uses it to get  more data from tv_details() 
+        # Gets the TMDB id for a series and uses it to get  more data from tv_details()
         elif self.category == "TV":
             # continue with additional requests
             self.id = r['tv_results'][0]['id']
@@ -43,62 +44,62 @@ class tmdb_api:
         del self.params['external_source']
 
     def movie_details(self, movie_id):
-      """
-      Returns the requested film's data.
-      Required: 
-        movie_id (string) from the TMDB API
-      Response:
-        The film's data in a dictionary containing the
-        title, overview, genres, popularity, language, 
-        country, runtimes, and status.
-      """
-      r = requests.get(
-          self.URL+f"movie/{movie_id}",
-          params=self.params
-      ).json()
+        """
+        Returns the requested film's data.
+        Required: 
+          movie_id (string) from the TMDB API
+        Response:
+          The film's data in a dictionary containing the
+          title, overview, genres, popularity, language, 
+          country, runtimes, and status.
+        """
+        r = requests.get(
+            self.URL+f"movie/{movie_id}",
+            params=self.params
+        ).json()
 
-      movie_info = {
-          "title": r['title'],
-          "overview": r['overview'],
-          "genres":  [x['name'] for x in r['genres']],
-          "popularity": r['popularity'],
-          "language": r['original_language'],
-          "country": r['production_companies'][0]['origin_country'],
-          "runtimes": r['runtime'],
-          "status": r['status'],
-      }
-      
-      return movie_info
+        movie_info = {
+            "title": r['title'],
+            "overview": r['overview'],
+            "genres":  [x['name'] for x in r['genres']],
+            "popularity": r['popularity'],
+            "language": r['original_language'],
+            "country": r['production_companies'][0]['origin_country'],
+            "runtimes": r['runtime'],
+            "status": r['status'],
+        }
+
+        return movie_info
 
     def tv_details(self, tv_id):  # get by tv_ID
-      """
-      Returns the requested tv series' data.
-      Required:
-        tv_id (string) from the TMDB API
-      Response:
-        The series' data in a dictionary containing the
-        title, overview, genres, popularity, language,
-        country, status, number of episodes, number of seasons, 
-        first airdate, lasgt airdate, and runtime.
-      """
-      r = requests.get(self.URL+f"tv/{tv_id}", params=self.params).json()
+        """
+        Returns the requested tv series' data.
+        Required:
+          tv_id (string) from the TMDB API
+        Response:
+          The series' data in a dictionary containing the
+          title, overview, genres, popularity, language,
+          country, status, number of episodes, number of seasons, 
+          first airdate, lasgt airdate, and runtime.
+        """
+        r = requests.get(self.URL+f"tv/{tv_id}", params=self.params).json()
 
-      tv_info = {
-          "title": r['name'],
-          "overview": r['overview'],
-          "genres": [x['name'] for x in r['genres']],
-          "popularity": r['popularity'],
-          "language": r['original_language'],
-          "country": r['origin_country'][0],
-          "status": r['status'],
-          "number_of_episodes": r['number_of_episodes'],
-          "number_of_seasons": r['number_of_seasons'],
-          "first_air_date": r['first_air_date'],
-          "last_air_date": r['last_air_date'],
-          "episode_run_time": r['episode_run_time'][0],
-      }
+        tv_info = {
+            "title": r['name'],
+            "overview": r['overview'],
+            "genres": [x['name'] for x in r['genres']],
+            "popularity": r['popularity'],
+            "language": r['original_language'],
+            "country": r['origin_country'][0],
+            "status": r['status'],
+            "number_of_episodes": r['number_of_episodes'],
+            "number_of_seasons": r['number_of_seasons'],
+            "first_air_date": r['first_air_date'],
+            "last_air_date": r['last_air_date'],
+            "episode_run_time": r['episode_run_time'][0],
+        }
 
-      return tv_info
+        return tv_info
 
     def get_season_ep(self, season_number):
         if self.category != "TV":
@@ -118,9 +119,11 @@ class tmdb_api:
 
 if __name__ == '__main__':
     # when called by itself, it shows the example tt4574334("stranger things")
-    strgr_things = tmdb_api(IMDB_ID_SAMPLE)
+    strgr_things = tmdb_api(IMDB_ID_SAMPLE,CATEGORY)
     show_details = strgr_things.find()
-    recent_season_eps = strgr_things.get_season_ep()
-
     print(show_details)
-    print(recent_season_eps)
+    print(show_details['number_of_seasons'])
+    recent_season_eps = strgr_things.get_season_ep(show_details['number_of_seasons'])
+    show_details['season_episodes'] = recent_season_eps
+    print(show_details)
+    
--- services/tmdb-api/tmdb_handler.py
+++ services/tmdb-api/tmdb_handler.py
@@ -2,32 +2,33 @@
 from tmdb_api import tmdb_api
 import time
 
-infile = 'tmdb_input.txt'
-outfile = 'tmdb_output.json'
+INFILE = 'tmdb_input.txt'
+OUTFILE = 'tmdb_output.json'
 
 while True:
+    time.sleep(1.0)
     # Checks if file is empty by converting string from file to list
-    if open(infile, "r").read().splitlines() == []:
-        print(False)
-        time.sleep(1.0)
-        continue
+    if open(INFILE, "r").read().splitlines() == []:
+      print(False)
+      continue
     
     # Saves the input from the file as a list [category, imdb_id]
-    with open(infile, "r+") as infile:
-        data = infile.read().splitlines()
-        infile.truncate(0)
-        infile.close()
+    with open(INFILE, "r+") as infile:
+      print(True)
+      data = infile.read().splitlines()
+      infile.truncate(0)
+      infile.close()
 
     # Calls the TMDB API with the saved inputs to get and write the film/series' data to a file
-    with open(outfile, "r+") as outfile:
-        info = str(tmdb_api(data[1], data[0]).find())
-        outfile.write(info)
-        outfile.close()
-    
+    with open(OUTFILE, "r+") as outfile:
+      info = str(tmdb_api(data[1], data[0]).find())
+      outfile.write(info)
+      outfile.close()
+  
     # Calls the TMDB API to append data to the file about the series' seasons 
     if data[1] == 'TV':
-        with open(outfile, "a") as outfile:
-            eps = str(tmdb_api(data[1], data[0]).get_season_ep())
-            outfile.write(eps)
-            outfile.close()
+      with open(OUTFILE, "a") as outfile:
+        eps = str(tmdb_api(data[1], data[0]).get_season_ep())
+        outfile.write(eps)
+        outfile.close()
 
