0b83bb5f33601eb1dd0169532033d333028bfe6a
encarnac <encarnac@oregonstate.edu>
2022-08-03 20:13:10 -0700

    Finished modal text layout

--- react-client/src/components/Card.js
+++ react-client/src/components/Card.js
@@ -1,4 +1,5 @@
 import { React, useState } from 'react';
+import Image from 'react-bootstrap/Image';
 import InfoModal from '../components/InfoModal';
 
 
@@ -15,8 +16,10 @@ function Card({content}) {
 
   return (
     <>
-      <div class="card bg-light bg-gradient bg-opacity-50 shadow mb-5 bg-body rounded-4 " style={{'width': '18rem;', 'background-image': 'linear-gradient(to left, #607076, #253B40);'}}>
-        <img src={content.poster} class="card-img-top img-fluid" alt="..."/>
+      <div class="card bg-light-gradient shadow mb-5 bg-body rounded-5" style={{'width': '18rem;', 'background-image': 'linear-gradient(to left, #607076, #253B40);'}}>
+        {/* <img src={content.poster} class="card-img-top img-fluid p-2 rounded-5" alt="..."/> */}
+                <Image src={content.poster} fluid='true'  rounded='true' class="card-img-top p-2 "/>
+
         <div class="card-body ">
           <h5 class="card-title">{content.title}</h5>
           { category === 'movie' ? 
@@ -35,6 +38,7 @@ function Card({content}) {
           overview={ content.overview }
           genres={ content.genres }
           popularity={ content.popularity }
+          release={content.release}
           firstRelease={ content.first_release }
           latestRelease={ content.latest_release }
           episodeCount={ content.episode_count }
--- react-client/src/components/InfoModal.js
+++ react-client/src/components/InfoModal.js
@@ -1,4 +1,7 @@
 import { React } from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image'
@@ -22,30 +25,117 @@ function InfoModal({ modalState,
 
 
   return (
-    <>
-      <Modal show={ modalState } onHide={ handleClose } >
+    <div class="h-50 d-inline-block">
+      <Modal show={ modalState } onHide={ handleClose } size="lg" centered>
         <Modal.Header closeButton>
           <Modal.Title>{ title }</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <Image src={poster} fluid='true' thumbnail='true' rounded='true' class="w-50 p-3"/>
-          <p><span>Overview: </span>{ overview }</p>
-          <p><span>Genres: </span>{ genres }</p>
-          <p><span>Popularity:</span>{ popularity }</p>
-          { category === 'movie' ?
-            <p><span>Release Date:</span>{ release }</p> :
-            <>
-              <p><span>First Release: </span>{ firstRelease }</p>
-              <p><span>Latest Release: </span>{ latestRelease }</p>
-              <p><span>Episode Count: </span>{ episodeCount }</p>
-              <p><span>Season Count: </span>{ seasonCount }</p>
-              <p><span>Newest Episodes: </span>{ seasonEpisodes }</p>
-            </>
-          }
-          
-          <p><span>Status: </span>{ status }</p>
-          
-          
+        <Modal.Body className="show-grid">
+          <Container>
+            <Row>
+              <Col xs={ 4 }>
+                <Image src={poster} fluid='true' rounded='true' />
+              </Col>
+              <Col sm={ 8 }>
+                <Row>
+                  <Col sm={6}>
+                    <h6>Overview:</h6>
+                  </Col>
+                  <Col sm={6}>
+                     <p>{overview}</p>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col sm={6}>
+                    <h6>Genres:</h6>
+                  </Col>
+                  <Col sm={6}>
+                     <p>{genres}</p>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col sm={6}>
+                    <h6>Popularity:</h6>
+                  </Col>
+                  <Col sm={6}>
+                     <p>{popularity}</p>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col sm={ 6 }>
+                    <h6>Status:</h6>
+                  </Col>
+                  <Col sm={ 6 }>
+                    <p>{ status }</p>
+                  </Col>
+                </Row>
+
+                { category === 'movie' ?
+                  <Row>
+                    <Col sm={ 6 }>
+                      <h6>Release Date:</h6>
+                    </Col>
+                    <Col sm={ 6 }>
+                      <p>{ release }</p>
+                    </Col>
+                  </Row> :
+
+                  <>
+                    <Row> 
+                      <Col sm={ 6 }>
+                        <h6>First Release:</h6>
+                      </Col>
+                      <Col sm={ 6 }>
+                        <p>{ firstRelease }</p>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col sm={ 6 }>
+                        <h6>Latest Release:</h6>
+                      </Col>
+                      <Col sm={ 6 }>
+                        <p>{ latestRelease }</p>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col sm={ 6 }>
+                        <h6>Episode Count:</h6>
+                      </Col>
+                      <Col sm={ 6 }>
+                        <p>{ episodeCount }</p>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col sm={ 6 }>
+                        <h6>Season Count:</h6>
+                      </Col>
+                      <Col sm={ 6 }>
+                        <p>{ seasonCount }</p>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col sm={ 6 }>
+                        <h6>New Episodes:</h6>
+                      </Col>
+                      <Col sm={ 6 }>
+                        <p>{ seasonEpisodes }</p>
+                      </Col>
+                    </Row>
+                  </> 
+                }
+
+
+
+              </Col>
+            </Row>
+          </Container>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={ handleClose }>
@@ -56,7 +146,7 @@ function InfoModal({ modalState,
           </Button>
         </Modal.Footer>
       </Modal>
-    </>
+    </div>
   );
 }
 
