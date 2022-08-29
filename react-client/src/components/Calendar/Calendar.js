7d90571f4174f95f7c3098a8d742b72434c9de4e
encarnac <76987299+encarnac@users.noreply.github.com>
2022-08-28 18:47:30 -0700

    Added new del multiple function to calendar

--- react-client/src/components/Calendar/Calendar.js
+++ react-client/src/components/Calendar/Calendar.js
@@ -1,11 +1,44 @@
 import { React, useState, useMemo, useEffect } from 'react';
-import EventsList from './EventsList';
+import Axios from 'axios';
+// import EventsList from './EventsList';
 import FullCalendar from '@fullcalendar/react'
 import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 
 function Calendar( { calendarId, events, fetchEvents } ) {
+
+    const [ selection, setSelected ] = useState([])
+
+
+    // Create CSS style and setstate toggle for select/unselect
+    const selectEvent = (e) => {
+        e.jsEvent.preventDefault();
+        setSelected(list => [...list, e.event])
+        e.el.style.backgroundColor = '#F1F1F1';
+        console.log(selection)
+    }
+    
+    const deleteEvents = async() => {
+        try {
+            for (const event of selection) {
+                const eventId = event.id
+                const delRes = await Axios.delete( '/delete-event', { 
+                    data: {   
+                        calendarId: calendarId,
+                        eventId: eventId
+                    }
+                }); 
+                event.remove()
+                setSelected([])
+            }
+        } catch (error) {
+            console.error(error)
+        } finally {
+            fetchEvents()
+        }
+    }
+
 
     return (
         <>
@@ -19,10 +52,38 @@ function Calendar( { calendarId, events, fetchEvents } ) {
                     {calendarId && Array.isArray(events) 
                         ? <FullCalendar
                                 plugins={[ listPlugin, bootstrap5Plugin, interactionPlugin ]}
-                                initialView="listWeek"
+                                initialView="listMonth"
                                 themeSystem= 'bootstrap5'
-                                aspectRatio='0.7'
-                                events={ events } />
+                                aspectRatio='0.8'
+                                titleFormat= {{ 
+                                    year: '2-digit', 
+                                    month: 'short', 
+                                    day: '2-digit' 
+                                }}
+                                headerToolbar={{
+                                    left:'title',
+                                    center:'today', 
+                                    right:'prev,next'
+                                }}
+                                footerToolbar={{
+                                    center: 'deleteButton'
+                                }}
+                                buttonIcons={{ 
+                                    prev: 'arrow-left-short', 
+                                    next: 'arrow-right-short'
+                                }}
+                                displayEventTime={false}
+                                eventInteractive={true}
+                                selectable={true}
+                                events={ events }
+                                eventClick={(e) => selectEvent(e)}
+                                customButtons={{
+                                    deleteButton: {
+                                    text: 'Delete selected',
+                                    click: () => deleteEvents()
+                                    }
+                                }}
+                                />
                         : <p>No upcoming releases found. <br /> You must link a Google Account.</p>
                     }
                 </div>
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
