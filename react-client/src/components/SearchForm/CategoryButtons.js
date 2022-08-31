9861abcc9168d02a3f91f59a93810f9468baf68b
encarnac <76987299+encarnac@users.noreply.github.com>
2022-08-31 15:19:59 -0700

    Fixed button resize bug

--- react-client/src/components/SearchForm/CategoryButtons.js
+++ react-client/src/components/SearchForm/CategoryButtons.js
@@ -42,20 +42,20 @@ function CategoryButtons( { props } ) {
                         <ul className='nav nav-pills gap-2'>
                             <li className='nav-item pill-1'>
                                 { display && !loading 
-                                ?   <button className='nav-link px-5 py-2 opacity-25' onClick={ selectSeries } disabled>
+                                ?   <button className='nav-link px-4 px-md-5 py-2 active opacity-25' onClick={ selectSeries } disabled>
                                         tv
                                     </button>
-                                :   <button className='nav-link px-5 py-2' onClick={ selectSeries }>
+                                :   <button className='nav-link px-4 px-md-5 py-2 active' onClick={ selectSeries }>
                                         tv
                                     </button>
                                 }                            
                             </li>
                             <li className='nav-item pill-1'>
                                 { display && !loading 
-                                ?   <button className='nav-link px-4 py-2 active opacity-25' onClick={ selectMovie } disabled>
+                                ?   <button className='nav-link px-3 px-md-4 py-2 opacity-25' onClick={ selectMovie } disabled>
                                         movie
                                     </button>
-                                :   <button className='nav-link px-4 py-2 active' onClick={ selectMovie }>
+                                :   <button className='nav-link px-3 px-md-4 py-2' onClick={ selectMovie }>
                                         movie
                                     </button>
                                 }   
