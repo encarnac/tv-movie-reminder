378208f1a4499b9ee8497bc1dfd25073f552b367
encarnac <76987299+encarnac@users.noreply.github.com>
2022-08-30 20:38:32 -0700

    Added selection count and clear button functions

--- react-client/src/components/Calendar/Calendar.js
+++ react-client/src/components/Calendar/Calendar.js
@@ -9,20 +9,26 @@ import bootstrap5Plugin from '@fullcalendar/bootstrap5';
 function Calendar( { calendarId, events, fetchEvents } ) {
 
     const [ selectedEvents, setSelectedEvents ] = useState([])
+    const [ selectedCount, setSelectedCount ] = useState(0)
 
-    const selectEvent = (e) => {
+    const selectEvent = async (e) => {
         e.jsEvent.preventDefault();
         const find = selectedEvents.some(selection => selection.id === e.event.id)
         console.log(find)
         if (find) {
             e.el.style.backgroundColor = '#F8F8F8';
             e.el.style.fontWeight = '400';
+            e.el.style.color='rgb(33,37,41)'
             const unselect = selectedEvents.filter(selection => selection.id !== e.event.id)
+            setSelectedCount(selectedCount-1)
             setSelectedEvents(unselect)
+            
         } else {
-            e.el.style.backgroundColor = '#F1F1F1';
+            e.el.style.backgroundColor = '#G1G1G1';
             e.el.style.fontWeight = '600';
-            setSelectedEvents(list => [...list, e.event])
+            e.el.style.color='#69A6E2';
+            const update = await setSelectedEvents(list => [...list, e.event])
+            setSelectedCount(selectedCount+1)
         }
     }
     
@@ -38,10 +44,17 @@ function Calendar( { calendarId, events, fetchEvents } ) {
                 }); 
                 event.remove() 
             }
+            setSelectedCount(0)
             setSelectedEvents([])
         } catch (error) {
             console.error(error)
         } 
+    }
+
+    const clearSelection = () => {
+        setSelectedCount(0)
+        setSelectedEvents([])
+        fetchEvents()
     }
 
 
@@ -55,40 +68,47 @@ function Calendar( { calendarId, events, fetchEvents } ) {
                 </div>
                 <div className='offcanvas-body opacity-75 px-4'>
                     {calendarId && Array.isArray(events) 
-                        ? <FullCalendar
-                                plugins={[ listPlugin, bootstrap5Plugin, interactionPlugin ]}
-                                initialView="listMonth"
-                                themeSystem= 'bootstrap5'
-                                aspectRatio='0.8'
-                                titleFormat= {{ 
-                                    year: '2-digit', 
-                                    month: 'short', 
-                                    day: '2-digit' 
-                                }}
-                                headerToolbar={{
-                                    left:'title',
-                                    center:'today', 
-                                    right:'prev,next'
-                                }}
-                                footerToolbar={{
-                                    center: 'deleteButton'
-                                }}
-                                buttonIcons={{ 
-                                    prev: 'arrow-left-short', 
-                                    next: 'arrow-right-short'
-                                }}
-                                displayEventTime={false}
-                                eventInteractive={true}
-                                selectable={true}
-                                events={ events }
-                                eventClick={(e) => selectEvent(e)}
-                                customButtons={{
-                                    deleteButton: {
-                                    text: 'Delete selected',
-                                    click: () => deleteEvents()
-                                    }
-                                }}
-                                />
+                        ?   <div className='container'>
+                                <FullCalendar
+                                    plugins={[ listPlugin, bootstrap5Plugin, interactionPlugin ]}
+                                    initialView="listMonth"
+                                    themeSystem= 'bootstrap5'
+                                    aspectRatio='0.68'
+                                    titleFormat= {{ 
+                                        year: '2-digit', 
+                                        month: 'short', 
+                                        day: '2-digit' 
+                                    }}
+                                    headerToolbar={{
+                                        left:'title',
+                                        center:'today', 
+                                        right:'prev,next'
+                                    }}
+                                    buttonIcons={{ 
+                                        prev: 'arrow-left-short', 
+                                        next: 'arrow-right-short'
+                                    }}
+                                    displayEventTime={false}
+                                    eventInteractive={true}
+                                    selectable={true}
+                                    events={ events }
+                                    eventClick={(e) => selectEvent(e)}
+                                    />
+                                <div className='row mt-3'>
+                                    <div className='col-4 my-2 ps-3'>
+                                        <h6>Selected:</h6>
+                                    </div>
+                                    <div className='col-1 my-2 pe-2'>
+                                        <span>{selectedCount}</span>
+                                    </div>
+                                    <div className='col-7 pe-3'>
+                                        <button className='btn btn-clear-input ms-3' onClick={ ()=> clearSelection() }>
+                                            clear</button>
+                                        <button className='btn btn-delete-input ms-2' onClick={ ()=>deleteEvents() }>
+                                            delete all</button>
+                                    </div>
+                                </div>
+                              </div>
                         : <p>No upcoming releases found. <br /> You must link a Google Account.</p>
                     }
                 </div>
