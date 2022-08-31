e3e2c6f8ca26083b68e29723c937b9250a943bb0
encarnac <76987299+encarnac@users.noreply.github.com>
2022-08-31 04:52:24 -0700

    New clear search/scroll up button

--- react-client/src/views/Dashboard.js
+++ react-client/src/views/Dashboard.js
@@ -15,7 +15,7 @@ function Dashboard( props ) {
     const selectSeries = () => {
         setCategory( 'tv' );
     };
-    
+
     const [ title, setTitle ] = useState( '' );
 
     const handleTitle = ( e ) => {
@@ -36,49 +36,72 @@ function Dashboard( props ) {
     const handleFetchResults = useCallback( () => {
         setLoading( true );
         Axios.get( url )
-        .then( response => {
-            setDisplay( !display );
-            setInputState( !inputState );
-            setTmdbData( response.data );
-            console.log( response.data );
-            setLoading( false );
-        })
-        .catch( error => {
-            console.log( error );
-        });
+            .then( response => {
+                setDisplay( !display );
+                setInputState( !inputState );
+                setTmdbData( response.data );
+                console.log( response.data );
+                setLoading( false );
+            } )
+            .catch( error => {
+                console.log( error );
+            } );
     }, [ url ] );
 
     useEffect( () => {
         handleFetchResults();
     }, [ handleFetchResults ] );
 
+    const [ clearButtonState, setClearButtonState ] = useState( false );
+
+    useEffect( () => {
+        window.addEventListener( 'scroll', () => {
+            if ( window.scrollY > 150 ) {
+                setClearButtonState( true );
+            } else {
+                setClearButtonState( false );
+            }
+        } );
+    }, [] );
+
     const clearResults = () => {
         setDisplay( false );
         setTmdbData( [] );
         setInputState( true );
+        window.scrollTo( {
+            top: 0,
+            behavior: 'smooth',
+        } );
     };
 
     return (
         <>
             <section className='header'>
-                < Header {...{ category, selectMovie, selectSeries, inputState, handleTitle, display, loading, handleURL }} />
+                < Header { ...{ category, selectMovie, selectSeries, inputState, handleTitle, display, loading, handleURL } } />
             </section>
 
             <section className='results'>
                 { !loading && display && (
-                    <div className='container mt-4'>
-                        <div className='row no-gutters d-flex gap-3 justify-content-center' >
-                            <svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='rgba(165,217,208, 0.95)' className='bi bi-x-circle-fill opacity-75' viewBox='0 0 10 50' onClick={ clearResults }>
-                                <path className='shadow-lg' d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z' />
-                            </svg>
-                            
-                            <DisplayResults 
-                                tmdbData={ tmdbData } 
+
+                    <div className='container mt-4 pt-5'>
+                        <div className='row no-gutters d-flex gap-2 justify-content-center' >
+                            <DisplayResults
+                                tmdbData={ tmdbData }
                                 calendarId={ props.calendarId }
                                 fetchEvents={ props.fetchEvents } />
                         </div>
+
+                        { clearButtonState && (
+                            <div className='fixed-bottom no-gutters'>
+                                <button className='btn btn-clear-results fw-semibold position-absolute bottom-0 start-0 mb-5'
+                                    onClick={ clearResults }>
+                                        clear search &nbsp;&nbsp;&nbsp;&nbsp;
+                                        <span className='bi bi-x-lg'></span>
+                                </button>
+                            </div>
+                        ) }
                     </div>
-                )}
+                ) }
             </section>
         </> );
 }
