99cc1cdcc6bf8220cf22906d2eae82bd11d842a7
encarnac <76987299+encarnac@users.noreply.github.com>
2022-09-12 14:42:40 -0700

    Added PORT env to google endpoints

--- server/api/google-api/controllers/loginUser.js
+++ server/api/google-api/controllers/loginUser.js
@@ -3,11 +3,13 @@ const { google } = require('googleapis');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const PORT = process.env.PORT;
+
 
 const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
     CLIENT_SECRET,
-    'http://localhost:5000'
+    `http://localhost:${PORT}`
 );
 
 const loginUser = async (req, res, next) => {
--- server/api/google-api/middleware/verifySession.js
+++ server/api/google-api/middleware/verifySession.js
@@ -4,11 +4,12 @@ const calendar = google.calendar('v3');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const PORT = process.env.PORT;
 
 const oauth2Client = new google.auth.OAuth2(
     CLIENT_ID,
     CLIENT_SECRET,
-    'http://localhost:5000'
+    `http://localhost:${PORT}`
 );
 
 const verifySession = async (req, res, next) => {
